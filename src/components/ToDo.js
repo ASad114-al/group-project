57e5b8956c357e69bf3316f130336be2f847c2e1
Finnschy <skylerfearless@yahoo.de>
2020-11-30 14:13:17 +0100

    react done

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -2,39 +2,44 @@ import React, { Component } from 'react';
 class ToDo extends Component {
     state = {
         taskInput: "",
-        collection: []
-
+        toDoList: []
     }
     handleChange = (event) => {
         this.setState({ [event.target.name]: event.target.value });
     }
     handleAdd = () => {
         let newObj = { tasks: this.state.taskInput }
-        let tempCollection = this.state.collection
-        tempCollection.push(newObj)
+        let temptoDoList = this.state.toDoList
+        temptoDoList.push(newObj)
         this.setState({
-            collection: tempCollection,
+            toDoList: temptoDoList,
             taskInput: "",
-            numTodos: this.state.taskInput.length
         });
     }
-    handleDelete = () => {
-        
+    handleDelete = (index) => {
+        let { toDoList } = this.state;
+        toDoList.splice(index, 1);
+        this.setState({
+            toDoList: toDoList
+        })
     }
     render() {
         return (
             <section>
-                <h1>To Do List {this.state.numTodos}</h1>
+                <h1>To Do List</h1>
+                <h2>You have {this.state.toDoList.length === 1 ? (this.state.toDoList.length + " task") : (this.state.toDoList.length + " tasks")} to do.</h2>
                 <form action="">
                     <input type="text" name="taskInput" value={this.state.taskInput} placeholder="Enter Task" onChange={this.handleChange} />
                 </form>
                 <button onClick={this.handleAdd}>Add Task</button>
-                {this.state.collection.map((ele, i) =>
-                    <div key={i}>
-                        <p>{ele.tasks}</p>
-                        <button onClick={this.handleDelete}>X</button>
-                    </div>)}
-            </section>
+                {
+                    this.state.toDoList.map((ele, i) =>
+                        <div key={i}>
+                            <p>{ele.tasks}</p>
+                            <button onClick={this.handleDelete}>X</button>
+                        </div>)
+                }
+            </section >
         );
     }
 }
